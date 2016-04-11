85d8e01a3c49638f3b1c73d49e94bfc8974bebe3
Ciara Merrins <ciaramerrins@gmail.com>
2016-04-11 23:22:40 +0100

    Project

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
 before_action :authenticate_admin!, except: [:index, :show]
+attr_accessor :body
 def index
 @posts = Post.all.order('created_at DESC')
 
@@ -14,6 +15,13 @@ end
 def show
 @post = Post.find(params[:id])
 end
+#memento pattern
+#def initialize body
+ #   @body = body
+  #end
+  def restore_save version
+    @body = version.body
+  end
 private
 def post_params
 params.require(:post).permit(:body)
